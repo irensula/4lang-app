74f5f7689d0684ad245e93a956ffe4350fca6eed
irensula <irinawebdev@gmail.com>
2024-07-28 18:19:40 +0300

    Add files via upload

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,8 @@
-// import right from "../data/task_1";
-
 const Task = () => {
     
     const right = ["ipsum", "libero"];
     const [right_word_1, right_word_2] = right;
-    
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const word_1 = e.target.word_1.value;
@@ -12,10 +10,8 @@ const Task = () => {
 
         if(word_1 === right_word_1 && word_2 === right_word_2) {
             console.log("Correct!");
-
-        }
     }
-
+    }
     return ( 
         <form onSubmit={handleSubmit}>
             <p className="taskText">Lorem</p>
@@ -23,23 +19,23 @@ const Task = () => {
                 type="text"
                 name="word_1"
                 className="taskInput"
-                
+                style={{color: word_1 === "libero" ? "trueColor" : "falseColor"}} 
             />
             <p className="taskText">dolor sit amet consectetur, adipisicing elit. Fuga aperiam animi voluptas tempore consequuntur exercitationem, suscipit sunt rem dicta similique. </p>
             <br/>
             <input 
                 type="text"
                 name="word_2"
-                className="taskInput"
-                
+                className="taskInput"              
+                style={{color: word_2 === "libero" ? "trueColor" : "falseColor"}}  
             />
                 <p className="taskText"> dolorum id velit ut, quod ab quis illo quaerat!</p>
             <br/>
-            <button className="button">Submit</button> 
+            <button 
+                className="button" 
+            >Submit</button> 
         </form> 
     );
 }
  
 export default Task;
-
-// styles
