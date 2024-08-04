ef4075e5fdcda6987e43d7dc510ede93d5422668
irensula <irinawebdev@gmail.com>
2024-08-04 11:39:52 +0300

    contacts page was changed

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,6 +1,8 @@
 const Contacts = () => {
     return ( <main>
         <h1>Contacts</h1>
+        <p>Email</p>
+        <p>irensula@gmail.com</p>
         <p>Phone number</p>
         <p>+358 40 8137886</p>
     </main> );
